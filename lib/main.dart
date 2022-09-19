ffb3aab7c03b2bcb0c3d8bdc18d3b6e83a78149b
Kaito Dogi <kendobu0405@gmail.com>
2022-09-20 03:33:50 +0900

    feat: HandlingGesturesApp を表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_to_widgets/basic_widgets.dart';
+import 'package:introduction_to_widgets/handing_gestures.dart';
 import 'package:introduction_to_widgets/hello_world.dart';
 import 'package:introduction_to_widgets/my_app.dart';
 import 'package:introduction_to_widgets/using_material_components.dart';
@@ -8,5 +9,6 @@ void main() {
   // runApp(const MyApp());
   // runApp(const HelloWorldApp());
   // runApp(const BasicWidgetsApp());
-  runApp(const UsingMaterialComponentsApp());
+  // runApp(const UsingMaterialComponentsApp());
+  runApp(const HandlingGesturesApp());
 }
