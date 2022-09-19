0b4603604cbf42f0aef49edf5d4d4042183eb99e
Kaito Dogi <kendobu0405@gmail.com>
2022-09-20 03:13:09 +0900

    feat: UsingMaterialComponentsApp を表示

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:introduction_to_widgets/basic_widgets.dart';
 import 'package:introduction_to_widgets/hello_world.dart';
 import 'package:introduction_to_widgets/my_app.dart';
+import 'package:introduction_to_widgets/using_material_components.dart';
 
 void main() {
   // runApp(const MyApp());
   // runApp(const HelloWorldApp());
-  runApp(const BasicWidgetsApp());
+  // runApp(const BasicWidgetsApp());
+  runApp(const UsingMaterialComponentsApp());
 }
